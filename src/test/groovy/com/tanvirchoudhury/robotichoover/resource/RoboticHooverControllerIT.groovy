b930455b19184aa73c4c82b19e8d875502984aee
Tanvir <TChoudhury@kcom.com>
2017-09-30 19:52:34 +0100

    Add IT to test whole application, that given an input the expected output is returned

--- src/test/groovy/com/tanvirchoudhury/robotichoover/resource/RoboticHooverControllerIT.groovy
+++ src/test/groovy/com/tanvirchoudhury/robotichoover/resource/RoboticHooverControllerIT.groovy
@@ -1,14 +1,48 @@
 package com.tanvirchoudhury.robotichoover.resource
 
-import groovyx.net.http.RESTClient
+import com.tanvirchoudhury.robotichoover.RoboticHooverApplication
+import com.tanvirchoudhury.robotichoover.model.dto.CleanEnvironmentResultDto
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.embedded.LocalServerPort
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
 
+import static com.tanvirchoudhury.robotichoover.fixtures.UncleanEnvironmentDtoFixtures.aUncleanEnvironmentDto
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+
+@ContextConfiguration
+@SpringBootTest(classes = [RoboticHooverApplication.class], webEnvironment = RANDOM_PORT)
 class RoboticHooverControllerIT extends Specification {
 
-    RESTClient client
+    @LocalServerPort
+    private int port
 
-    def setup(){
-        client = new RESTClient("http://localhost:8080/robotic-hoover/")
+    @Autowired
+    TestRestTemplate testRestTemplate
+
+    def "Clean environment"() {
+
+        given: "An input"
+        def uncleanEnvDto = aUncleanEnvironmentDto()
+
+        when: "When a request is sent"
+        ResponseEntity result = testRestTemplate.postForEntity("http://localhost:$port/robotic-hoover/clean",
+                uncleanEnvDto,
+                CleanEnvironmentResultDto.class)
+
+        then: "Expected result is returned"
+        result.statusCode == HttpStatus.OK
+
+        def cleanEnvironmentResultDto = result.getBody()
+        cleanEnvironmentResultDto.getCoords()[0] == 1
+        cleanEnvironmentResultDto.getCoords()[1] == 3
+
+        cleanEnvironmentResultDto.patches == 1
+
     }
 
 }
